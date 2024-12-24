a7a0db968b28b30c760f749617a3c03fd1a969d6
Daniel Sanfelix <dsanfealos@gmail.com>
2024-12-24 17:30:43 +0100

    Finished HUD 4 (Cost of upgrading Arm) and HUD 5 (Cost of upgrading P Organ). Still need error handling for HUD 5

---
+++ src/main/java/calculator/countsOfP/exceptions/NotEnoughModulesException.java
@@ -0,0 +1,8 @@
+package calculator.countsOfP.exceptions;
+
+public class NotEnoughModulesException extends RuntimeException{
+
+    public NotEnoughModulesException (String message){
+        super(message);
+    }
+}
